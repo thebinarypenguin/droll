4dac1bac630a61938d0cf43335657fe06a282309
Ethan Zimmerman <thebinarypenguin@gmail.com>
2013-12-14 20:03:47 -0500

    Create a "release" grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,17 +26,32 @@ module.exports = function(grunt) {
         reporter: 'spec'
       },
       all: ['test/*.js']
+    },
+
+    // Bump, Tag, Push
+    bump: {
+      options: {
+        updateConfigs: ['pkg'],
+        commitFiles: ['-a'],
+        pushTo: 'origin'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-mocha-cli');
-
-  grunt.registerTask('lint',   ['jshint']);
-  grunt.registerTask('minify', ['uglify']);
-  grunt.registerTask('test',   ['mochacli']);
+  grunt.loadNpmTasks('grunt-bump');
 
   grunt.registerTask('default', ['jshint', 'mochacli']);
+  grunt.registerTask('lint',    ['jshint']);
+  grunt.registerTask('minify',  ['uglify']);
+  grunt.registerTask('test',    ['mochacli']);
 
+  grunt.registerTask("release", "Release a new version, push it and publish it", function(target) {
+    if (!target) {
+      target = "patch";
+    }
+    return grunt.task.run("bump-only:" + target, "uglify", "bump-commit");
+  });
 };
